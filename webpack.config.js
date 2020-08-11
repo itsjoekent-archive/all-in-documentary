55d35877e51c1e4f49520aa7172c0b65d64a8955
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2020-08-10 22:55:34 -0400

    bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './main.css',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: process.env.PRODUCTION ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //     },
+      //   },
+      // },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
