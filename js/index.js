0dc6b8cd8f06521441b8a654b63abcfac001fe6e
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2020-08-14 20:46:44 -0400

    Navigation

--- js/index.js
+++ js/index.js
@@ -10,4 +10,20 @@ import '../css/main.scss';
   document.getElementById('close-trailer').addEventListener('click', function() {
     trailer.classList.remove('--open');
   });
+
+  let isNavigationOpen = false;
+  const navigation = document.getElementById('navigation');
+  const toggleNavigation = document.getElementById('toggle-navigation');
+
+  toggleNavigation.addEventListener('click', function() {
+    isNavigationOpen = !isNavigationOpen;
+
+    if (isNavigationOpen) {
+      navigation.classList.add('--open');
+      toggleNavigation.textContent = 'Close';
+    } else {
+      navigation.classList.remove('--open');
+      toggleNavigation.textContent = 'Menu';
+    }
+  });
 })();
