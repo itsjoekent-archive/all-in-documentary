43306e02fa6ed52f02f1d0bdeac1c33a6f3b1104
22 Pines <70674428+22-pines@users.noreply.github.com>
2020-09-08 12:10:58 +0530

    new page development
    new page development

--- webpack.config.js
+++ webpack.config.js
@@ -88,6 +88,10 @@ module.exports = {
       template: 'html/mail-in-ballot.html',
       filename: 'mail-in-ballot.html',
     }),
+    new HtmlWebpackPlugin({
+      template: 'html/new-page.html',
+      filename: 'new-page.html',
+    }),
     new CopyPlugin({
       patterns: [
         { from: 'static', to: 'static' },
