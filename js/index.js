715fdd4092556304a6834d23cf525eb628aaf65a
22 Pines <70674428+22-pines@users.noreply.github.com>
2020-09-08 12:15:21 +0530

    added node fetch module and custom css changes from new page
    added node fetch module and custom css changes from new page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import '../css/main.scss';
-
+var $ = require('jQuery');
 (function () {
   const trailer = document.getElementById('trailer');
   const openTrailer = document.getElementById('open-trailer');
@@ -32,4 +32,38 @@ import '../css/main.scss';
       toggleNavigation.textContent = 'Menu';
     }
   });
+
+  var sheet_url = "https://spreadsheets.google.com/feeds/list/1LoXeop63we3nr8tmkFSV8Z0YWiB0zmPzYxtJqEB939k/od6/public/values?alt=json"; 
+  $(document).ready(function(){
+    const fetch = require('node-fetch');
+
+    let url = sheet_url;
+
+    let settings = { method: "Get" };
+    var $table = $("#location-data")
+    fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+        // do something with JSON
+        var entry = json.feed.entry;
+        var tr = "<tr>";
+        $(entry).each(function(){
+          if(this.gsx$date.$t != null || this.gsx$date.$t != "" || this.gsx$date.$t != "null"){ 
+           if(this.gsx$city.$t != null || this.gsx$city.$t != "" || this.gsx$city.$t != "null"){
+             if(this.gsx$state.$t != null || this.gsx$state.$t != "" || this.gsx$state.$t != "null"){
+               if(this.gsx$address.$t != null || this.gsx$address.$t != "" || this.gsx$address.$t != "null"){
+                 if(this.gsx$time.$t != null || this.gsx$time.$t != "" || this.gsx$time.$t != "null"){
+                  tr +="<td>"+this.gsx$date.$t+"</td><td>"+this.gsx$city.$t+"</td><td>"+this.gsx$state.$t+"</td><td>"+this.gsx$address.$t+"</td><td>"+this.gsx$time.$t+"</td>";
+                }
+              }
+            }
+          }
+        }
+        tr += "</tr>";
+      });
+        $table.find('tbody').empty().append(tr);
+      });
+    
+  })
+
 })();
